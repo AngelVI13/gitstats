fee5d69e18a4f69a353bcd07b913064c81199260
AngelVI <a.v.iliev13@gmail.com>
2023-08-25 09:57:06 +0300

    changes

--- bin/main.ml
+++ bin/main.ml
@@ -1,30 +1,28 @@
-let maybe_read_line () =
-    try Some(read_line ())
-    with End_of_file -> None
-
+let maybe_read_line () = try Some (read_line ()) with End_of_file -> None
 
 let rec read_stdin lines =
-    match maybe_read_line () with
-    (* this does not use tail recursion and its not very performant because of it *)
-    | Some(line) -> read_stdin (lines @ [line]) (* for better perf: line :: lines *)
-    | None -> lines
+  match maybe_read_line () with
+  (* this does not use tail recursion and its not very performant because of it *)
+  | Some line ->
+      read_stdin (lines @ [ line ]) (* for better perf: line :: lines *)
+  | None -> lines
 
-let rec find_commits lines commit_idxs idx =
-    match lines with 
-    | [] -> commit_idxs
-    | (hd :: tl) -> begin
-        let commit_idxs = 
-            if String.starts_with ~prefix:"commit " hd then 
-                (idx :: commit_idxs) 
-            else commit_idxs in
-        find_commits tl commit_idxs (idx+1)
-    end
+let rec find_commits lines commits =
+  match lines with
+  | [] -> commits
+  | hd :: tl ->
+      print_endline hd;
+      let commits =
+        match Scanf.sscanf_opt hd "commit %S" Fun.id with
+        | None -> commits
+        | Some c ->
+            Printf.printf "%s" c;
+            c :: commits
+      in
+      find_commits tl commits
 
-let find_commit_idxs lines = 
-    find_commits lines [] 0
+let find_commits_new lines = find_commits lines []
 
-let () = 
-    let commit_idxs = [] |> read_stdin |> find_commit_idxs in
-    List.iter (Printf.printf "%d ") commit_idxs;
-    (* how best to do string processing ? *)
-    print_endline ""
+let () =
+  let commits = [] |> read_stdin |> find_commits_new in
+  List.iter (Printf.printf "%s ") commits
