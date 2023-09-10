585fe8e75c1e43c7cb1c9ec85d3e9214f5196e2c
AngelVI <a.v.iliev13@gmail.com>
2023-09-10 15:09:49 +0300

    support oneline

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,44 @@
+(*
 let maybe_read_line () = try Some (read_line ()) with End_of_file -> None
+*)
 
+(*
 let rec read_stdin lines =
   match maybe_read_line () with
-  (* this does not use tail recursion and its not very performant because of it *)
-  | Some line ->
-      read_stdin (lines @ [ line ]) (* for better perf: line :: lines *)
+  | Some line -> read_stdin (lines @ [ line ]) 
   | None -> lines
+*)
 
-let rec find_commits lines commits =
-  match lines with
-  | [] -> commits
-  | hd :: tl ->
-      let commits =
-        match Scanf.sscanf_opt hd "commit %s" Fun.id with
-        | None -> commits
-        | Some c -> c :: commits in
-      find_commits tl commits
+let test =
+  "704800d8d (HEAD -> develop, tag: Sprint_Releases/S54_July_21_2023, \
+   origin/develop, origin/HEAD) Merged PR 3787: VL 52 fix for 4AP2-44364\n\
+   4e06e981d updated docstrings\n\
+   32d86fffa Apply suggestions from code review"
 
-let find_commits_new lines = find_commits lines []
+(*
+let print_lines lst =
+    let out = List.mapi (
+        fun i el -> 
+            Printf.sprintf "%d, %s\n" i el 
+    ) lst in 
+    String.concat " " out |> print_endline
+*)
+
+type commit_info = { revision : string; msg : string }
+
+let print_commits lst =
+  let out =
+    List.mapi
+      (fun i el -> Printf.sprintf "%d, %s %s\n" i el.revision el.msg)
+      lst
+  in
+  String.concat " " out |> print_endline
+
+let rec records_of_lines lst =
+  match lst with
+  | [] -> []
+  | hd :: tl -> { revision = hd; msg = "fake" } :: records_of_lines tl
 
 let () =
-  let commits = [] |> read_stdin |> find_commits_new in
-  List.iter (Printf.printf "%s ") commits
+  let commits = String.split_on_char '\n' test |> records_of_lines in
+  print_commits commits
